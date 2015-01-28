03da913dbbf5740387be6a20f7b8375c87bf6339
triston <triston@users.noreply.github.com>
2015-01-29 02:53:28 +0800

    Update share_links.js

--- assets/javascripts/share_links.js
+++ assets/javascripts/share_links.js
@@ -1,17 +1,19 @@
 
 import Sharing from 'discourse/lib/sharing';
-Sharing.addSource({
-    // This id must be present in the `share_links` site setting too
-    id: 'weibo',
-
-    // The icon that will be displayed
-    iconClass: 'fa-weibo',
-
-    generateUrl: function(link, title) {
-        return "http://service.weibo.com/share/share.php?url=" + encodeURIComponent(link) + "&title=" + encodeURIComponent(title);
-    },
-      
-    shouldOpenInPopup: true,
-    popupHeight: 370
-});
+(function($) {
+    Sharing.addSource({
+        // This id must be present in the `share_links` site setting too
+        id: 'weibo',
+    
+        // The icon that will be displayed
+        iconClass: 'fa-weibo',
+    
+        generateUrl: function(link, title) {
+            return "http://service.weibo.com/share/share.php?url=" + encodeURIComponent(link) + "&title=" + encodeURIComponent(title);
+        },
+          
+        shouldOpenInPopup: true,
+        popupHeight: 370
+    });
+})(jQuery);
 
